de57562eec8a420c3e52e948af19f6f5e4b5a4b5
NIH896 <hah018272@gmail.com>
2025-09-18 12:25:23 +0900

    first commit

---
+++ src/Event.js
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+/*function Event() {
+
+    const { eventId } = useParams();
+    const [items, setItems] = useState([]);
+    const [title, setTitle] = useState("");
+    const [price, setPrice] = useState("");
+
+    useEffect(() => {
+        //データベースからデータ取得する.
+        const postData = collection(db, "posts");
+        //初回データ取得
+        getDocs(postData).then((snapShot) => {
+        //console.log(snapShot.docs.map((doc) => ({ ...doc.data() })));
+        setPosts(snapShot.docs.map((doc) => ({ ...doc.data() })))
+        });
+
+        //リアルタイムで取得
+        onSnapshot(postData, (post) => {
+        setPosts(post.docs.map((doc) => ({ ...doc.data() })));
+        });
+
+        // ページ読み込み時に localStorage から復元
+        
+        const savedPost = localStorage.getItem("lastPost");
+        if (savedPost) {
+        const { title, text } = JSON.parse(savedPost);
+        setTitle(title);
+        setText(text);
+        }
+    }, []);
+
+    }*/
